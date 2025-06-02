c1248b1991558f71383150a04e938119e9f5b7ca
Dexter Docallas <ddocallas1108@gmail.com>
2025-06-02 17:17:46 +0800

    feat: added click event listener

--- Drum_Kit/Drum Kit Starting Files/index.js
+++ Drum_Kit/Drum Kit Starting Files/index.js
@@ -1,8 +1,16 @@
 let numberOFDrumButtons = document.querySelectorAll('.drum').length
+let audio = new Audio();  
 
 for (let i = 0; i < numberOFDrumButtons; i++){
   document.querySelectorAll('.drum')[i].addEventListener('click',function handleClick() {
-    alert('I got clicked!')
+    console.log(this.innerHTML)
+    if (this.innerHTML === w) {
+      audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+    } else {
+      audio = new Audio('sounds/crash.mp3');
+      audio.play
+    }
   });
 }
 
