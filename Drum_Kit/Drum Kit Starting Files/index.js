23c8172f82cbe42ad04985b1bc02dae1ed0134a3
Dexter Docallas <ddocallas1108@gmail.com>
2025-06-02 18:11:25 +0800

    feat: finished drum kit

--- Drum_Kit/Drum Kit Starting Files/index.js
+++ Drum_Kit/Drum Kit Starting Files/index.js
@@ -2,16 +2,47 @@ let numberOFDrumButtons = document.querySelectorAll('.drum').length
 let audio = new Audio();  
 
 for (let i = 0; i < numberOFDrumButtons; i++){
-  document.querySelectorAll('.drum')[i].addEventListener('click',function handleClick() {
-    console.log(this.innerHTML)
-    if (this.innerHTML === w) {
-      audio = new Audio('sounds/kick-bass.mp3');
-      audio.play();
-    } else {
-      audio = new Audio('sounds/crash.mp3');
-      audio.play
-    }
+  document.querySelectorAll('.drum')[i].addEventListener('keypress',function(event) {
+    console.log(this.innerHTML);
+
+    drumSound(this.innerHTML);
   });
 }
 
-  
+document.addEventListener('keypress', function(event) {
+  drumSound(event.key);
+});
+
+function drumSound(key) {
+
+  switch (key) {
+    case 'w':
+      audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+      break;
+    case 'a':
+      audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
+      break;
+    case 's':
+      audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+      break;
+    case 'd':
+      audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+      break;
+    case 'j':
+      audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+      break;
+    case 'k':
+      audio = new Audio('sounds/crash.mp3');
+      audio.play();
+      break;
+    case 'l':
+      audio = new Audio('sounds/snare.mp3');
+      audio.play();
+      break;
+  }
+}
